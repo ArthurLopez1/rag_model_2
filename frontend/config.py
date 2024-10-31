bd890894361a48d3c3f7d8fb4100e10b9aa82019
Arthur_Lopez <arthur.lopez@outlook.com>
2024-10-31 15:44:23 +0100

    new protoype "local_rag_2"

---
+++ frontend/config.py
@@ -0,0 +1,10 @@
+import logging
+
+# Streamlit page configuration
+STREAMLIT_PAGE_CONFIG = {
+    "page_title": "Ollama PDF RAG Streamlit ",
+    "page_icon": "🎈",
+    "layout": "wide",
+    "initial_sidebar_state": "collapsed",
+}
+
